d95bfbc58fca01ecc710868339354d8f1f3b2bda
JR_RUEL <blxier.odaroma@gmail.com>
2023-01-12 21:28:16 +0800

    scrollspy added

---
+++ progs.js
@@ -0,0 +1,40 @@
+function Menu(e) {
+    let list = document.querySelector('ul');
+
+    e.name === 'menu' ? (e.name = "close",list.classList.add('opacity-100')) 
+    : (e.name = "menu", list.classList.remove('opacity-100'))
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    
+    let home = document.getElementById('home-btn');
+    let about = document.getElementById('about-btn');
+    let contact = document.getElementById('contact-btn');
+    
+  if (document.documentElement.scrollTop > 1 && document.documentElement.scrollTop <= 400 ) {
+    home.classList.add('bg-secondary-color');
+  }
+  else if(document.documentElement.scrollTop > 400){
+    home.classList.remove('bg-secondary-color');
+  }
+
+  if (document.documentElement.scrollTop > 500 && document.documentElement.scrollTop <= 1100 ) {
+    about.classList.add('bg-secondary-color');
+  }
+  else if(document.documentElement.scrollTop > 1100 || document.documentElement.scrollTop < 500){
+    about.classList.remove('bg-secondary-color');
+  }
+
+  if (document.documentElement.scrollTop > 1100) {
+    contact.classList.add('bg-secondary-color');
+  }
+  else if(document.documentElement.scrollTop < 1100){
+    contact.classList.remove('bg-secondary-color');
+  }
+
+
+
+
+}
